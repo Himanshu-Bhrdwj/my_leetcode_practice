ef476086f9f1172570a0711264b7b1879a3201ee
Himanshu-Bhrdwj <himanshuatmbd@gmail.com>
2026-07-05 22:39:16 +0530

    Time: 0 ms (100%), Space: 14.2 MB (33.51%) - LeetHub

---
+++ 1089-duplicate-zeros/1089-duplicate-zeros.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        int n = arr.size();
+
+      for (int i = 0; i < n; i++)
+      {
+        if (arr[i] == 0)
+        {
+            arr.insert(arr.begin() + i, 0);
+            arr.pop_back();
+            i = i + 1;
+            continue;
+      }
+    }
+
+    }
+};
